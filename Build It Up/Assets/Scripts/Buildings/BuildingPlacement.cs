81c9a5b98415008263db7e8ea5efe9380d4d068d
UG-Andi <hirzberger.ah@hotmail.com>
2016-02-01 17:52:21 +0100

    first commit

---
+++ Build It Up/Assets/Scripts/Buildings/BuildingPlacement.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingPlacement : MonoBehaviour {
+
+    private PlaceableBuildings placeableBuilding;
+    private PlaceableBuildings placeableBuildingOld;
+    private Transform currentBuilding;
+    private bool hasPlaced;
+
+    public LayerMask buildingsMask;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 m = Input.mousePosition;
+        m = new Vector3(m.x, m.y, transform.position.y);
+        Vector3 p = GetComponent<Camera>().ScreenToWorldPoint(m);
+
+        if (currentBuilding != null && hasPlaced == false)
+        {
+            currentBuilding.position = new Vector3(Mathf.Round(p.x / 5) * 5, 0, Mathf.Round(p.z / 5) * 5);
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (IsLegalPosition())
+                {
+                hasPlaced = true;
+                }
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit = new RaycastHit();
+                Ray ray = new Ray(new Vector3(p.x, 10, p.z), Vector3.down);
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity, buildingsMask))
+                {
+                    if (placeableBuildingOld != null)
+                    {
+                        placeableBuildingOld.SetSelected(false);
+                    }
+                    hit.collider.gameObject.GetComponent<PlaceableBuildings>().SetSelected(true);
+                    placeableBuildingOld = hit.collider.gameObject.GetComponent<PlaceableBuildings>();
+                }
+
+                else
+                {
+                    if (placeableBuildingOld != null)
+                    {
+                    placeableBuildingOld.SetSelected(false);
+                    }
+                }
+            }
+        }
+	}
+
+    bool IsLegalPosition()
+    {
+        if (placeableBuilding.colliders.Count > 0)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    public void SetItem(GameObject b)
+    {
+        hasPlaced = false;
+        currentBuilding = ((GameObject)Instantiate(b)).transform;
+        placeableBuilding = currentBuilding.GetComponent<PlaceableBuildings>();
+    }
+}
---
+++ Build It Up/Assets/Scripts/Buildings/PlaceableBuildings.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlaceableBuildings : MonoBehaviour {
+
+    [HideInInspector]
+    public List<Collider> colliders = new List<Collider>();
+    private bool isSelected;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnGUI()
+    {
+        if (isSelected)
+        {
+            GUI.Button(new Rect(Screen.width/2, Screen.height/5, 100, 30), name);
+        }
+    }
+
+
+    void OnTriggerEnter(Collider c)
+    {
+        if (c.tag == "Building")
+        {
+            colliders.Add(c);
+        }
+    }
+
+    void OnTriggerExit(Collider c)
+    {
+        if (c.tag == "Building")
+        {
+            colliders.Remove(c);
+        }
+    }
+
+    public void SetSelected(bool s)
+    {
+        isSelected = s;
+    }
+}
