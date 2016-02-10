39d0157393bed4281b3cdf4f769790bb2da2d1fd
UG-Andi <hirzberger.ah@hotmail.com>
2016-02-11 00:09:31 +0100

    cube upgrade

--- Build It Up/Assets/Scripts/Buildings/BuildingUpgradeCube.cs
+++ Build It Up/Assets/Scripts/Buildings/BuildingUpgradeCube.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class BuildingUpgradeCube : MonoBehaviour {
 
-    public static float cooldown;
+    public float cooldown;
     private float timer;
+
+    public GameObject cubeTier2;
 
 	// Use this for initialization
 	void Start ()
     {
-	    
+        timer = 10;
 	}
 	
 	// Update is called once per frame
@@ -17,7 +19,7 @@ public class BuildingUpgradeCube : MonoBehaviour {
     {
         if (timer <= 0)
         {
-
+            Upgrade();
             timer = cooldown;
         }
         else
@@ -28,6 +30,52 @@ public class BuildingUpgradeCube : MonoBehaviour {
 
     void Upgrade()
     {
+        if (gameObject.name == "Cube Tier 1(Clone)")
+        {
+            Vector3 minPosition = GetComponent<Collider>().bounds.min;
+            Vector3 size = GetComponent<Collider>().bounds.size;
 
+            if (!Physics.Raycast(minPosition + new Vector3(size.x * 0.25f, 0, 0), Vector3.back, 4) &&
+                !Physics.Raycast(minPosition + new Vector3(size.x * 0.75f, 0, 0), Vector3.back, 4))
+            {
+                Vector3 position2 = transform.position + new Vector3(0, 0, -2.5f);
+                Destroy(gameObject);
+                Instantiate(cubeTier2, position2, Quaternion.Euler(0, 90, 0));
+            }
+            else
+            {
+                if (!Physics.Raycast(minPosition + new Vector3(size.x, 0, size.z * 0.25f), Vector3.right, 4) &&
+                    !Physics.Raycast(minPosition + new Vector3(size.x, 0, size.z * 0.75f), Vector3.right, 4))
+                {
+                    Vector3 position2 = transform.position + new Vector3(2.5f, 0, 0);
+                    Destroy(gameObject);
+                    Instantiate(cubeTier2, position2, Quaternion.Euler(0, 0, 0));
+                }
+                else
+                {
+                    if (!Physics.Raycast(minPosition + new Vector3(size.x * 0.25f, 0, size.z), Vector3.forward, 4) &&
+                        !Physics.Raycast(minPosition + new Vector3(size.x * 0.75f, 0, size.z), Vector3.forward, 4))
+                    {
+                        Vector3 position2 = transform.position + new Vector3(0, 0, 2.5f);
+                        Destroy(gameObject);
+                        Instantiate(cubeTier2, position2, Quaternion.Euler(0, -90, 0));
+                    }
+                    else
+                    {
+                        if (!Physics.Raycast(minPosition + new Vector3(0, 0, size.z * 0.25f), Vector3.left, 4) &&
+                            !Physics.Raycast(minPosition + new Vector3(0, 0, size.z * 0.75f), Vector3.left, 4))
+                        {
+                            Vector3 position2 = transform.position + new Vector3(-2.5f, 0, 0);
+                            Destroy(gameObject);
+                            Instantiate(cubeTier2, position2, Quaternion.Euler(0, 180, 0));
+                        }
+                        else
+                        {
+                            Debug.Log("no upgrade");
+                        }
+                    }
+                }   
+            }
+        }        
     }
 }
